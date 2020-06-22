0f949a5fb09eaa547894c63187ce5e4421995543
HenockAdane <65575319+HenockAdane@users.noreply.github.com>
2020-06-22 16:54:44 +0100

    Update script.js
    fixed the code

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ var toggleBtn = document.querySelector("#toggleBtn");
 let navBar = document.querySelector("#nav-bar");
 
 toggleBtn.addEventListener("click", function(){
-    if(navBar.style.display = "none"){
+
+    if(navBar.style.display === "none")
+    {
         return navBar.style.display = "block"
     }
 
-    else{ return navBar.style.display = "none"}
+    else
+    {
+        return navBar.style.display = "none"
+    }
 })
