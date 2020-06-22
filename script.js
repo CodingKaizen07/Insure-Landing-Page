5f0b3643451bae8c1ae271263030ed555c8965be
HenockAdane <65575319+HenockAdane@users.noreply.github.com>
2020-06-22 13:48:29 +0100

    adding files

---
+++ script.js
@@ -0,0 +1,10 @@
+var toggleBtn = document.querySelector("#toggleBtn");
+let navBar = document.querySelector("#nav-bar");
+
+toggleBtn.addEventListener("click", function(){
+    if(navBar.style.display = "none"){
+        return navBar.style.display = "block"
+    }
+
+    else{ return navBar.style.display = "none"}
+})
